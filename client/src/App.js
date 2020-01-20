9c1a00c8f09415c1e2a4e88086097b6d02ea3805
lianadanae <lianabette@gmail.com>
2020-01-20 18:18:58 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import Search from './pages/Search/Search.js';
+import Saved from './pages/Saved/Saved.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <React.Fragment>
+        <Switch>
+          {/* <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} /> */}
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </React.Fragment>
+    </Router>
+  );
 }
 
 export default App;
